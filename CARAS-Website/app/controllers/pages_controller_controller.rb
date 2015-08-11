4482e8ff717cbeaa6e9233a5513db41aeffc3739
Rachelle Tanase <rachelle.tanase@yahoo.com>
2015-08-10 21:29:51 -0700

    Made navbar and part of header

--- CARAS-Website/app/controllers/pages_controller_controller.rb
+++ CARAS-Website/app/controllers/pages_controller_controller.rb
@@ -1,14 +1,31 @@
 class PagesControllerController < ApplicationController
-	def about_us
+	# Definte english pages
+	def about_us_en
 	end
-	def meet_the_team
+	def meet_the_team_en
 	end
-	def events
+	def events_en
+		@events = Event.all
 	end
-	def contact_us
+	def contact_us_en
 	end
-	def gallery
+	def gallery_en
 	end
-	def blog
+	def blog_en
+	end
+
+	# Define spanish pages
+	def about_us_es
+	end
+	def meet_the_team_es
+	end
+	def events_es
+		@events = Event.all
+	end
+	def contact_us_es
+	end
+	def gallery_es
+	end
+	def blog_es
 	end
 end
--- CARAS-Website/db/migrate/20150811012112_create_events.rb
+++ CARAS-Website/db/migrate/20150811012112_create_events.rb
@@ -1,8 +1,17 @@
 class CreateEvents < ActiveRecord::Migration
   def change
+  	#image1 and image2 will be paths to image files
     create_table :events do |t|
       t.text :name
       t.text :description
+      t.date :date
+      t.text :street_address
+      t.text :city
+      t.text :zip_code
+      t.text :state
+      t.text :image1
+      t.text :image2
+      t.text :language
       t.timestamps null: false
     end
   end
