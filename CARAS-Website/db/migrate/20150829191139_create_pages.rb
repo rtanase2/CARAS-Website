9499a891d5c5dee26d5fd2d1e2f29ca83b167f36
Rachelle Tanase <rachelle.tanase@yahoo.com>
2015-08-29 16:40:49 -0700

    Removed title from page model

--- CARAS-Website/db/migrate/20150829191139_create_pages.rb
+++ CARAS-Website/db/migrate/20150829191139_create_pages.rb
@@ -2,7 +2,6 @@ class CreatePages < ActiveRecord::Migration
   def change
     create_table :pages do |t|
         t.text :innerHTML
-        t.string :title
         t.string :lang
         t.string :pageType
     end
