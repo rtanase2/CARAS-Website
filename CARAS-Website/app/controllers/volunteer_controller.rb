b202db701a30203e5b78453590286c9a2d5f2d7b
frgperez <frgperez@ucsc.edu>
2015-08-29 19:27:53 -0700

    Volunteer pre over haul
    Volunteer page I was working on before the overhaul by Rachelle. Was
    looking into making the webpage form and saving the data into the
    database.

--- CARAS-Website/app/controllers/volunteer_controller.rb
+++ CARAS-Website/app/controllers/volunteer_controller.rb
@@ -1,2 +1,19 @@
 class VolunteerController < ApplicationController
+	def new
+		@volunteer = Volunteer.new
+	end
+	
+	def create
+		@volunteer = Volunteer.new(volunteer_params)
+		if @volunteer.save
+			redirect_to '/volunteer-en'
+		else
+			render 'new'
+		end
+	end
+	
+	private
+		def  volunteer_params
+			params.require(:volunteer).permit(:name)
+		end
 end
