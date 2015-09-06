513d346cdb3449c079c9bac8ca9c5e1f68ba2191
Rachelle Tanase <rachelle.tanase@yahoo.com>
2015-09-06 10:19:29 -0700

    Add location

--- CARAS-Website/app/assets/javascripts/tamale_fest_functions.js
+++ CARAS-Website/app/assets/javascripts/tamale_fest_functions.js
@@ -41,26 +41,7 @@ $(document).ready(function(){
     // Handle english cases
       case "descripción":
       case "description":
-        var dateArr = e.date.split("-");
-        var formattedDate = dateArr[1] + "/" + dateArr[2] + "/" + dateArr[0];
-        var startTime = e.start_time.slice(11, 16);
-        if ("12:00" > startTime){
-          startTime += "AM"
-        } else if ("12:00" <= startTime && "12:59" >= startTime){
-          startTime += "PM"
-        } else {
-          startTime = startTime.slice(0, 2)%12 + startTime.slice(2) + "PM"
-        }
-        var endTime = e.end_time.slice(11, 16);
-        if ("12:00" > endTime){
-          endTime += "AM"
-        } else if ("12:00" <= endTime && "12:59" >= endTime){
-          endTime += "PM"
-        } else {
-          endTime = endTime.slice(0, 2)%12 + endTime.slice(2) + "PM"
-        }
-        tabContainer.append("<h3><b>Date:</b> " + formattedDate + "</h3><h3><b>Time:</b> " + startTime +  " - " + endTime + "</h3>");
-        tabContainer.append("<h3><b>Location:</b> " + + "</h3>");
+        generateDescriptionContent(e);
         break;
       case "esponsor":
       case "sponsors":
@@ -84,24 +65,52 @@ $(document).ready(function(){
 
 function createSponsorsTable(e){
   var sponsors = $('#invisible').data('sponsors');
-        var currRow = 0;
-        tabContainer = $('#tab-container')
-        tabContainer.append("<table><tbody id=\"logo-table\"><tr id=\"" + 
-          "tr" + currRow + "\">");
-        var numLogos = 0;
-        table = $('#logo-table');
-        row = $('#tr' + currRow);
-        for (s in e.sponsors){
-          if ($('body').width() < (numLogos+1)*200){
-            currRow++;
-            table.append("</tr><tr id=\"" + 
-          "tr" + currRow + "\">");
-            numLogos = 0;
-            row = $('#tr' + currRow);
-          }
-          row.append("<td><h3>   <img width=\"150px\" src=\"" + sponsors[parseInt(e.sponsors[s])].logo_url + 
-            "\" alt=\"" + sponsors[parseInt(e.sponsors[s])].name + "\"></img></h3></td>");
-          numLogos++;
-        }
-        tabContainer.append("</tr></tbody></table>");
+  var currRow = 0;
+  tabContainer = $('#tab-container');
+  tabContainer.append("<table><tbody id=\"logo-table\"><tr id=\"" + 
+    "tr" + currRow + "\">");
+  var numLogos = 0;
+  table = $('#logo-table');
+  row = $('#tr' + currRow);
+  for (s in e.sponsors){
+    if ($('body').width() < (numLogos+1)*200){
+      currRow++;
+      table.append("</tr><tr id=\"" + 
+    "tr" + currRow + "\">");
+      numLogos = 0;
+      row = $('#tr' + currRow);
+    }
+    row.append("<td><h3>   <img width=\"150px\" src=\"" + sponsors[parseInt(e.sponsors[s])].logo_url + 
+      "\" alt=\"" + sponsors[parseInt(e.sponsors[s])].name + "\"></img></h3></td>");
+    numLogos++;
+  }
+  tabContainer.append("</tr></tbody></table>");
+}
+
+function generateDescriptionContent(e){
+  tabContainer = $('#tab-container');
+  var dateArr = e.date.split("-");
+  var formattedDate = dateArr[1] + "/" + dateArr[2] + "/" + dateArr[0];
+  var startTime = e.start_time.slice(11, 16);
+  if ("12:00" > startTime){
+    startTime += "AM"
+  } else if ("12:00" <= startTime && "12:59" >= startTime){
+    startTime += "PM"
+  } else {
+    startTime = startTime.slice(0, 2)%12 + startTime.slice(2) + "PM"
+  }
+  var endTime = e.end_time.slice(11, 16);
+  if ("12:00" > endTime){
+    endTime += "AM"
+  } else if ("12:00" <= endTime && "12:59" >= endTime){
+    endTime += "PM"
+  } else {
+    endTime = endTime.slice(0, 2)%12 + endTime.slice(2) + "PM"
+  }
+  tabContainer.append("<h3><b>Date:</b> " + formattedDate + 
+                      "</h3><h3><b>Time:</b> " + startTime +  " - " + 
+                      endTime + "</h3>");
+  tabContainer.append("<h3><b>Location:</b> " + e.street_address + ", " + 
+                      e.city + ", " + e.state + " " + e.zip_code 
+                      + "</h3>");
 }
