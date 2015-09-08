0e0cb8c386da01a3d91552b5c7e9223422759101
Rachelle Tanase <rachelle.tanase@yahoo.com>
2015-09-08 13:36:29 -0700

    Removed El Charrito From Sponsors

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@
 
 Event.create(event_name: "Second Annual Tamale Festival",
              description: "Fun for the whole family! We are featuring an number of tamale vendors from different origins that you can purchase as well as providing entertainment thorughout the day. There will be dancers, a car show, a tamale cook off, musical performances and much more! Don't miss this great event!",
-             subheader: "Brought to you by CARAS and El Charrito Market",
+             subheader: "Brought to you by CARAS",
              date: DateTime.new(2015, 9, 27),
              start_time: DateTime.new(2015, 9, 27, 12, 00, 0),
              end_time: DateTime.new(2015, 9, 27, 19, 00, 0),
@@ -27,7 +27,7 @@ Event.create(event_name: "Second Annual Tamale Festival",
              img_takers_url: "https://www.flickr.com/photos/shaynabright/",
              img_takers_name: "shaynabright",
              img_license_url: "https://creativecommons.org/licenses/by-nc-nd/2.0/",
-             sponsors: ["0", "1"],
+             sponsors: ["0"],
              entertainment: [],
              state: "CA",
              language: "en",
@@ -35,7 +35,7 @@ Event.create(event_name: "Second Annual Tamale Festival",
              event_type: "tamale-fest")
 
 Event.create(event_name: "Segundo Festival del Tamal",
-             subheader: "Traído a usted por CARAS y El Charrito Mercado",
+             subheader: "Traído a usted por CARAS",
              description: "¡Diversión para toda la familia! Estamos ofreciendo un número de vendedores de tamales de diferentes orígenes que usted puede comprar , así como proporcionar entretenimiento thorughout el día. Habrá bailarines , una exhibición de autos , un tamal Cook Off , actuaciones musicales y mucho más ! No te pierdas este gran evento!",
              date: DateTime.new(2015, 9, 27),
              start_time: DateTime.new(2015, 9, 27, 12, 00, 0),
@@ -47,7 +47,7 @@ Event.create(event_name: "Segundo Festival del Tamal",
              img_takers_url: "https://www.flickr.com/photos/shaynabright/",
              img_takers_name: "shaynabright",
              img_license_url: "https://creativecommons.org/licenses/by-nc-nd/2.0/",
-             sponsors: ["0", "1"],
+             sponsors: ["0"],
              entertainment: [],
              state: "CA",
              language: "es",
@@ -60,6 +60,3 @@ Event.create(event_name: "Segundo Festival del Tamal",
 
 Sponsor.create(name: "CARAS",
                logo_url: "caras-logo-transparent.png")
-
-Sponsor.create(name: "El Charrito Market",
-               logo_url: "El Charrito logo.png")
