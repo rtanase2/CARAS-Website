a0a9e73f14afd5511c2e8fa777a900b042898621
Rachelle Tanase <rachelle.tanase@yahoo.com>
2015-09-08 19:01:21 -0700

    Seeded all sponsors AND sponsorships

--- db/migrate/20150816163117_create_sponsors.rb
+++ db/migrate/20150816163117_create_sponsors.rb
@@ -1,7 +1,7 @@
 class CreateSponsors < ActiveRecord::Migration
   def change
     create_table :sponsors do |t|
-      t.string :name
+      t.string :sponsor_name
       t.string :logo_url
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -70,10 +70,10 @@ ActiveRecord::Schema.define(version: 20150908213718) do
   end
 
   create_table "sponsors", force: :cascade do |t|
-    t.string   "name"
+    t.string   "sponsor_name"
     t.string   "logo_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "sponsorships", force: :cascade do |t|
